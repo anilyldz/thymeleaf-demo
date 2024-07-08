bc5cbcc63b359c896a625754d666cf5676d0a4e9
anilyildiz <anil-yildiz@outlook.com.tr>
2024-07-08 10:02:32 +0300

    Injecting Drop down list values from application.properties

--- src/main/java/com/anily/thymeleaf_demo/controller/StudentController.java
+++ src/main/java/com/anily/thymeleaf_demo/controller/StudentController.java
@@ -1,19 +1,28 @@
 package com.anily.thymeleaf_demo.controller;
 
 import com.anily.thymeleaf_demo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+
+    @Value("${countries}")
+    private List<String> countries;
+
 
     @GetMapping("/showStudentForm")
     public String showStudentForm(Model model){
         Student student = new Student();
         model.addAttribute("student", student);
+        model.addAttribute("countries", countries);
+
         return "student-form";
     }
 
